dafc2faaf4dae08553c39666db1dedcb1d7c54a6
alena505 <115369290+alena505@users.noreply.github.com>
2025-11-16 17:48:49 +0300

    Update KLResultAnalyticalMethod.cs

--- BooleanCompletenessBack/Models/KLResultAnalyticalMethod.cs
+++ BooleanCompletenessBack/Models/KLResultAnalyticalMethod.cs
@@ -5,17 +5,12 @@
 
         public string SourceExpression { get; set; }
 
-        //1)Расркрываем все операции кроме И ИЛИ НЕ
         public List<ExpressionChange> RevealingComplexOperators { get; set; }
 
-        //2)Переводим в ДНФ
         public List<ExpressionChange> DnfRetrieving { get; set; }
-
-        //3)Преобразуем ДНФ в СДНФ
 
         public List<ExpressionChange> PdnfRetrieving { get; set; }
 
-        //4)Преобраование СДНФ в АНФ(Полином Жегалкина)
         public List<ExpressionChange> AnfRetrieving { get; set; }
 
     }
