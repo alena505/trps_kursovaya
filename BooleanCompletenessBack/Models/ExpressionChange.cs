d9f50a4259639f257a9beb9b467125dbb1ae2a4e
alena505 <115369290+alena505@users.noreply.github.com>
2025-11-16 17:48:24 +0300

    Update ExpressionChange.cs

--- BooleanCompletenessBack/Models/ExpressionChange.cs
+++ BooleanCompletenessBack/Models/ExpressionChange.cs
@@ -1,13 +1,10 @@
 ﻿namespace BooleanCompletenessBack.Models
 {
 
-    //Описание изменения выражения
     public class ExpressionChange
     {
-        //Полученное выражение
         public string Expression { get; set; }
-
-        //Причина изменения выражения
+        
         public string Reason { get; set; }
 
         public ExpressionChange(string reason, string expression)
